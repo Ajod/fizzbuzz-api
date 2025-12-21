d4cc8831c0ec1b3d636b89527318dc56427d0c33
Alexis Jodar <alexisjodar@gmail.com>
2025-12-22 00:41:53 +0100

    removed unused signal catching

--- cmd/fizzbuzz-api/main.go
+++ cmd/fizzbuzz-api/main.go
@@ -1,31 +1,17 @@
 package main
 
 import (
-	"context"
 	"fizzbuzz-api/internal/fizzbuzzapi/http"
 	"fizzbuzz-api/internal/fizzbuzzapi/logger"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 func main() {
 	logger := logger.NewSlogLogger()
 
-	ctx, stop := signal.NotifyContext(
-		context.Background(),
-		os.Interrupt,
-		syscall.SIGTERM,
-	)
-	defer stop()
-
-	go func() {
-		server, err := http.NewServer(logger)
-		if err != nil {
-			panic(err)
-		}
-		server.Run()
-	}()
-	<-ctx.Done()
+	server, err := http.NewServer(logger)
+	if err != nil {
+		panic(err)
+	}
+	server.Run()
 	logger.Info("shutting down fizzbuzz-api")
 }
